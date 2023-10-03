97f2f712b9461532101dc1ca95c28186247e2461
Khanh-09 <145407768+Khanh-09@users.noreply.github.com>
2023-10-03 09:47:52 +0700

    Add files via upload

---
+++ btth03/app/controllers/SongController.php
@@ -0,0 +1,11 @@
+<?php
+require_once APP_ROOT.'/app/services/SongService.php';
+class SongController{
+    public function index(){
+        $songService = new SongService();
+        $songs = $songService->getAllSongs();
+
+        include APP_ROOT.'/app/views/home/index.php';
+    }
+}
+?>
